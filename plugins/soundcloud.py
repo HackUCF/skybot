e39def54eac5588aa1dfb7c7777f227d593c9451
broglea <austin.brogle@ucf.edu>
2013-11-20 13:15:48 -0500

    updated soundcloud to check regex for links

--- plugins/soundcloud.py
+++ plugins/soundcloud.py
@@ -1,10 +1,21 @@
 import soundcloud
 from util import hook
+import re
 
 # create a client object with your app credentials
 client = soundcloud.Client(client_id='APIKEY')
 
+soundcloud_re = (r'((https|http)?:\/\/(soundcloud.com)\/(.*))', re.I)
+
 @hook.command
 def soundcloud(search):
+	if(search==''):
+		return "Usage: .soundcloud *trackname*"
 	track = client.get('/tracks', q=search, order='hotness', streamable='true', limit=1)[0]
 	return track.title + " | " + track.permalink_url
+
+
+@hook.regex(*soundcloud_re)
+def soundcloud_url(match):
+	track = client.get('/resolve', url=match.group(0))
+	return track.title
