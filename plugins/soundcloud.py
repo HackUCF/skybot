f3b2fba32c1e2554114fecf79a7e1cff4cee9e47
broglea <austin.brogle@ucf.edu>
2013-11-20 15:52:07 -0500

    Updated soundcloud plugin to list user who updated sound as artist before song name. Also added .sc command hook for easier searches

--- plugins/soundcloud.py
+++ plugins/soundcloud.py
@@ -9,13 +9,19 @@ soundcloud_re = (r'((https|http)?:\/\/(soundcloud.com)\/(.*)\/(.*))', re.I)
 
 @hook.command
 def soundcloud(search):
-	if(search==''):
-		return "Usage: .soundcloud *trackname*"
-	track = client.get('/tracks', q=search, order='hotness', streamable='true', limit=1)[0]
-	return track.title + " | " + track.permalink_url
+        if(search==''):
+                return "Usage: .soundcloud *trackname*"
+        # find all sounds of buskers licensed under 'creative commons share alike'
+        track = client.get('/tracks', q=search, order='hotness', streamable='true', limit=1)[0]
+        return "*" + track.user["username"] + "*" + " | " + track.title + " | " + track.permalink_url
+
+@hook.command
+def sc(search):
+        return soundcloud(search)
 
 
 @hook.regex(*soundcloud_re)
 def soundcloud_url(match):
-	track = client.get('/resolve', url=match.group(0))
-	return track.title
+        #return match.group(0)
+        track = client.get('/resolve', url=match.group(0))
+        return "*" + track.user["username"] + "*" + " | " + track.title
