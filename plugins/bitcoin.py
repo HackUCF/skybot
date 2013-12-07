4a2ca8509d6b1189c727d433119486fcac0e7656
Mark Ignacio <mark.ignacio@knights.ucf.edu>
2013-12-07 09:31:12 -0500

    alias .buttcoin to .bitcoin

--- plugins/bitcoin.py
+++ plugins/bitcoin.py
@@ -1,6 +1,6 @@
 from util import http, hook
 
-
+@hook.command('buttcoin', autohelp=False)
 @hook.command(autohelp=False)
 def bitcoin(inp, say=None):
     ".bitcoin -- gets current exchange rate for bitcoins from mtgox"
