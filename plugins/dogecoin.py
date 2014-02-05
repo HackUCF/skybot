caee2210835a240f37a79291f4ed356b2c0b4ddd
Luis Santana <hacktalk@hacktalk.net>
2014-02-05 10:25:27 -0500

    Makes .doge output in USD

--- plugins/dogecoin.py
+++ plugins/dogecoin.py
@@ -3,9 +3,5 @@ from util import http, hook
 @hook.command(autohelp=False)
 def dogecoin(inp, say=None):
     ".dogecoin -- gets current exchange rate for dogecoins"
-    data = http.get_json("http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132")
-    data = data['return']
-    ticker = {
-        'btc': data['markets']['DOGE']['lasttradeprice'],
-    }
-    say("Current Doge: \x0307%(btc)s\x0f BTC" % ticker)
+    data = http.get_json("https://www.dogeapi.com/wow/?a=get_current_price&convert_to=USD&amount_doge=1")
+    say("Current Doge: \x0307%s\x0f USD" % data)
