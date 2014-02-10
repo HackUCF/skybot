094cdcf5552e3fd09bdadc47e7d0ac3d4f819ce8
JonathanSinger <nothingness00@gmail.com>
2014-02-10 16:08:35 -0500

    Dogecoin updated

--- plugins/dogecoin.py
+++ plugins/dogecoin.py
@@ -4,5 +4,4 @@ from util import http, hook
 def dogecoin(inp, say=None):
     ".dogecoin -- gets current exchange rate for dogecoins"
     data = http.get_json("https://www.dogeapi.com/wow/?a=get_current_price&convert_to=USD&amount_doge=1")
-    # say("Current Doge: \x0307%s\x0f USD" % data)
     say("Current Doge: \x0307%s\x0f USD Current MegaDoge: \x0307%s\x0f USD" % (data, data*100000))
