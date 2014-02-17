cd0e74880f0e25269b616e0c801a635ae8eea3ce
C0deH4cker <c0deh4cker@gmail.com>
2014-02-17 18:08:54 -0500

    Made some plugins more Pythonic

--- plugins/pyexec.py
+++ plugins/pyexec.py
@@ -12,10 +12,10 @@ def python(inp):
 
     res = http.get("http://eval.appspot.com/eval", statement=inp).splitlines()
 
-    if len(res) == 0:
+    if not res:
         return
     res[0] = re_lineends.split(res[0])[0]
-    if not res[0] == 'Traceback (most recent call last):':
+    if res[0] != 'Traceback (most recent call last):':
         return res[0].decode('utf8', 'ignore')
     else:
         return res[-1].decode('utf8', 'ignore')
--- plugins/wolframalpha.py
+++ plugins/wolframalpha.py
@@ -8,17 +8,17 @@ from util import hook, http
 @hook.command
 def wolframalpha(inp, api_key=None):
     ".wa/.wolframalpha <query> -- computes <query> using Wolfram Alpha"
-
+    
     url = 'http://api.wolframalpha.com/v2/query?format=plaintext'
-
+    
     result = http.get_xml(url, input=inp, appid=api_key)
-
+    
     pod_texts = []
     for pod in result.xpath("//pod"):
         title = pod.attrib['title']
         if pod.attrib['id'] == 'Input':
             continue
-
+        
         results = []
         for subpod in pod.xpath('subpod/plaintext/text()'):
             subpod = subpod.strip().replace('\\n', '; ')
@@ -27,24 +27,22 @@ def wolframalpha(inp, api_key=None):
                 results.append(subpod)
         if results:
             pod_texts.append(title + ': ' + '|'.join(results))
-
+    
     ret = '. '.join(pod_texts)
-
+    
     if not pod_texts:
         return 'no results'
-
+    
     ret = re.sub(r'\\(.)', r'\1', ret)
-
+    
     def unicode_sub(match):
         return unichr(int(match.group(1), 16))
-
+    
+    # Should probably be [0-9a-f], as it's parsed as a hexadecimal number in unicode_sub
     ret = re.sub(r'\\:([0-9a-z]{4})', unicode_sub, ret)
-
+    
     if len(ret) > 430:
         ret = ret[:ret.rfind(' ', 0, 430)]
         ret = re.sub(r'\W+$', '', ret) + '...'
-
-    if not ret:
-        return 'no results'
-
-    return ret
+    
+    return ret or 'no results'
