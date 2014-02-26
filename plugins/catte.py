0ee13c7c3c7980e3992e801b244fc30c4b49c9bc
Luis Santana <hacktalk@hacktalk.net>
2014-02-26 02:22:42 -0500

    Removes catte.py

--- plugins/catte.py
+++
@@ -1,9 +0,0 @@
-from util import http, hook
-import re
-
-@hook.command('catte', autohelp=False)
-@hook.command(autohelp=False)
-def cattes(inp, say=None):
-    ".cattes -- gets cate facts"
-    data = http.get_json("http://catfacts-api.appspot.com/api/facts")
-    say("%s" % data['facts'][0].replace("cat","catte"))
