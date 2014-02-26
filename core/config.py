2d0b8996862b5f5adb006cba5db9f625368b3315
Connection <hacktalkblog@gmail.com>
2014-02-26 01:39:52 -0500

    Updates config.py
    Fixes missing curly brace which was causing channel joining to fail

--- core/config.py
+++ core/config.py
@@ -24,6 +24,7 @@ if not os.path.exists('config'):
                 {
                   "name": "#passworded",
                   "password": "pword"
+                }
               ]
             }
           },
