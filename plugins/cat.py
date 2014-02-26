ac7d2b5dce7afc640590d8b31a6fe8902f9980ab
Luis Santana <hacktalk@hacktalk.net>
2014-02-26 02:21:51 -0500

    Melds .cat and .catte into one plugin and updates replacement

--- plugins/cat.py
+++ plugins/cat.py
@@ -1,8 +1,18 @@
 from util import http, hook
+import re
 
 @hook.command('cat', autohelp=False)
 @hook.command(autohelp=False)
-def cattes(inp, say=None):
-    ".cattes -- gets cate facts"
+def cat(inp, say=None):
+    ".cat -- gets cat facts"
     data = http.get_json("http://catfacts-api.appspot.com/api/facts")
     say("%s" % data['facts'][0])
+
+@hook.command('catte', autohelp=False)
+@hook.command(autohelp=False)
+def cattes(inp, say=None):
+    ".catte -- gets catte facts"
+    data = http.get_json("http://catfacts-api.appspot.com/api/facts")
+    catte = re.sub("cat", "catte", data['facts'][0])
+    catte = re.sub("Cat", "Catte", catte)
+    say("%s" % catte)
