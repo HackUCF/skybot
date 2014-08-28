2c8856ded819ca21d5d9f3b968c2fd6a956f0590
Cody <ctmskater75@gmail.com>
2014-08-27 20:49:17 -0400

    ninjafish command

--- plugins/bitcoin.py
+++ plugins/bitcoin.py
@@ -7,3 +7,11 @@ def bitcoin(inp, say=None):
     data = http.get_json("https://btc-e.com/api/2/btc_usd/ticker")
     say("BTC/USD: \x0307{buy:.0f}\x0f - High: \x0307{high}\x0f"
         " - Low: \x0307{low}\x0f - Volume: {vol_cur:.0f}".format(**data['ticker']))
+
+
+@hook.command('bitcat'. autohelp=False)
+@hook.command(autohelp=False)
+def bitcat(inp, say=None):
+    ".bitcat -- ninjafish morning"
+    bitcoin()
+    catte()
