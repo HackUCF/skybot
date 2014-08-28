a7f242d51e1a52f95272eb6a72ffd3feca4518ea
Cody <ctmskater75@gmail.com>
2014-08-28 15:49:25 -0400

    back

--- plugins/ninjafish.py
+++ plugins/ninjafish.py
@@ -1,8 +1,14 @@
 from util import http, hook  
 import re
 
+@hook.command('bitcat', autohelp=False)
 @hook.command(autohelp=False)
 def bitcat(inp, say=None):
     ".bitcat -- ninjafish morning"
-    bitcoin(None)
-    cattes(None)
+    data = http.get_json("https://btc-e.com/api/2/btc_usd/ticker")
+    say("BTC/USD: \x0307{buy:.0f}\x0f - High: \x0307{high}\x0f"
+        " - Low: \x0307{low}\x0f - Volume: {vol_cur:.0f}".format(**data['ticker']))
+    data = http.get_json("http://catfacts-api.appspot.com/api/facts")
+    catte = re.sub("cat", "catte", data['facts'][0])
+    catte = re.sub("Cat", "Catte", catte)
+    say("%s" % catte)
