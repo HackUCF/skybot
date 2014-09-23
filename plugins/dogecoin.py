0e1520f096ab29fdec55c2a7006628e6450bbe7d
JonathanSinger <nothingness00@gmail.com>
2014-09-22 22:22:25 -0400

    fix doge

--- plugins/dogecoin.py
+++ plugins/dogecoin.py
@@ -3,5 +3,8 @@ from util import http, hook
 @hook.command(autohelp=False)
 def dogecoin(inp, say=None):
     ".dogecoin -- gets current exchange rate for dogecoins"
-    data = http.get_json("https://www.dogeapi.com/wow/?a=get_current_price&convert_to=USD&amount_doge=1")
-    say("Current Doge: \x0307%s\x0f USD Current MegaDoge: \x0307%s\x0f USD" % (data, data*1000000))
+    data = http.get_json("http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=182")
+    data = data['return']
+    data = data['markets']
+    data = data['DOGE']
+    say("Current Doge: \x0307%s\x0f USD" % (data['lasttradeprice'])
